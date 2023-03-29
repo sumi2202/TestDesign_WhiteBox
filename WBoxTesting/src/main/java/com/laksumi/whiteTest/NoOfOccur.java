d9f1f5138f6f699e8f20619795f0273fc909e307
Laksumi Soma <laksumi2202@gmail.com>
2023-03-29 08:21:54 -0400

    .

--- WBoxTesting/src/main/java/com/laksumi/whiteTest/NoOfOccur.java
+++ WBoxTesting/src/main/java/com/laksumi/whiteTest/NoOfOccur.java
@@ -2,7 +2,17 @@ package com.laksumi.whiteTest;
 
 public class NoOfOccur {
     private static final int maxChar = 256;
-    int charContain[] = new int[maxChar];
+    static void checkOccur(String usrInput){
+        int charContain[] = new int[maxChar];
+        int strLength = usrInput.length();
+        char chara[] = new char[usrInput.length()];
 
-    
+        for(int i =0; i < strLength; i++){
+            charContain[usrInput.charAt(i)]++;
+        }
+        
+    }
+
+
+
 }
