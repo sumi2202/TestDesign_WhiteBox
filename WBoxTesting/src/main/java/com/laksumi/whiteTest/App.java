07dfb1c9b6083596d15b15785b4dae400a9dd1e9
Laksumi Soma <laksumi2202@gmail.com>
2023-03-29 08:36:33 -0400

    .

--- WBoxTesting/src/main/java/com/laksumi/whiteTest/App.java
+++ WBoxTesting/src/main/java/com/laksumi/whiteTest/App.java
@@ -1,13 +1,17 @@
 package com.laksumi.whiteTest;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner input = new Scanner(System.in);
+        String usrInput;
+
+        System.out.print( "Enter a word to start search: ");
+        usrInput = input.nextLine();
+
+        NoOfOccur.checkOccur(usrInput);
     }
 }
